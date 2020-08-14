560f7beea6c1e5dad97d1c86b510053c721a78b5
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-08-14 11:02:41 -0400

    Initial commit. Currently a dropdown menu will appear when typing in keywords for a movie delayed by 500 ms. Added Bulma for a CSS framework.

---
+++ README.md
@@ -0,0 +1,1 @@
+A web app that allows a user to compare movie stats.
---
+++ scripts/utils.js
@@ -0,0 +1,13 @@
+/* Delays a function call by specified delay in milliseconds. If no delay passed, default is 1000
+milliseconds.*/
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
